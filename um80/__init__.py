88ef58c991794ee0e5eda63d07ea81f76a05fad3
Aaron Wohl <wohl@awohl.com>
2025-11-26 22:52:58 +0000

    setup for PyPi

---
+++ um80/__init__.py
@@ -0,0 +1,14 @@
+"""
+um80 - Microsoft MACRO-80 compatible assembler toolchain for Linux.
+
+This package provides Unix/Linux implementations of the classic CP/M
+development tools from Microsoft:
+
+- um80: MACRO-80 compatible assembler
+- ul80: LINK-80 compatible linker
+- ulib80: LIB-80 compatible library manager
+- ucref80: Cross-reference utility
+- ud80: 8080/Z80 disassembler
+"""
+
+__version__ = "1.0.0"
